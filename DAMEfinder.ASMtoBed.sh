150e91c216173c5c7e17e80280e6cb2ca7c152c2
MykMalev <151015680+MykMalev@users.noreply.github.com>
2024-02-14 10:28:23 +0100

    Add files via upload

---
+++ DAMEfinder.ASMtoBed.sh
@@ -0,0 +1,31 @@
+#Reformat DAMEfinder calc_derivedasm output's derASM into a bedgraph format.
+
+asmtobed()
+{
+sed -i '1d' $1 ;
+awk -F'\t' '{gsub(/"/, "", $1); print $1, $2}' $1 > $1.tmp ;
+cut -d '.' -f 2 <$1.tmp > $1.tmp2 ;
+awk -v OFS='\t' '{ print $1 }' $1.tmp2 > $1.tmp3 ;
+awk -v OFS='\t' ' {$2=$1+1}{ print $1, $2 }' $1.tmp3 > $1.tmp4 ;
+awk -F'\t' -vOFS='\t' '{ $1 = "chr" $1 }1' $1.tmp > $1.tmp5 ;
+awk '{sub(/\..*$/, "", $1)} 1' $1.tmp5 > $1.tmp6 ;
+awk 'NR==FNR{a[NR]=$0;next}{print a[FNR],$0}' $1.tmp6 $1.tmp4 > $1.tmp7 ;
+awk -v OFS='\t' '{ print $1, $3 , $4, $2 }' $1.tmp7 > $1.derived.asm.bedgraph ;
+rm $1.tmp
+rm $1.tmp2
+rm $1.tmp3 
+rm $1.tmp4
+rm $1.tmp5
+rm $1.tmp6
+rm $1.tmp7
+}
+
+### For every text file in the current folder
+echo "ASMtoBed"
+for I in `ls *asm*` ;
+do 
+asmtobed $I
+done ;
+
+## For a specific input
+#asmtobed $1
